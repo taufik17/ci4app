3c792b7765db81dfcddb3c47ff8c5703d23a2403
Taufik Agung Santoso <taufikagungsantoso17@gmail.com>
2020-11-15 13:16:24 +0700

    model bag 1

--- ci4app/app/Views/layout/navbar.php
+++ ci4app/app/Views/layout/navbar.php
@@ -9,6 +9,7 @@
 				<a class="nav-link active" href="/">Home</a>
 				<a class="nav-link" href="/pages/about">About</a>
 				<a class="nav-link" href="/pages/contact">Contact</a>
+				<a class="nav-link" href="/komik">Komik</a>
 			</div>
 		</div>
 	</div>
